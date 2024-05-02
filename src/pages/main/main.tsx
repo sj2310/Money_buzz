936deac07a86707dcd1b3c559a04d26b757f06db
Sajal Jain <sajaljn23@gmail.com>
2024-05-02 23:28:00 +0530

    login error on main page

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -16,14 +16,18 @@ export interface Post {
 
 const Main = () => {
   const [postList, setPostList] = useState<Post[] | null>(null);
-  const postRef = collection(db, "posts");
-  const [user]= useAuthState(auth)
+  const postRef = collection(db, "posts")
+  const [user] = useAuthState(auth);
 
   const getPosts = async () => {
-    const data = await getDocs(postRef);
-    setPostList(
-      data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[]
-    );
+    try {
+      const data = await getDocs(postRef);
+      setPostList(
+        data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[]
+      );
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
   };
 
   useEffect(() => {
@@ -35,13 +39,16 @@ const Main = () => {
     <div className="main-box">
      <span className="welcome">Welcome to Money Market</span> 
       <div className="post-container">
-        {user ? postList && postList.map(post => (
-          <PostBox key={post.id} post={post} />
-        )):<p>LogIn To Continue</p>}
+        {user ? (
+          postList && postList.map(post => (
+            <PostBox key={post.id} post={post} />
+          ))
+        ) : (
+          <p>LogIn To Continue</p>
+        )}
       </div>
     </div>
   );
-  // return<>ed</>
 };
 
 export default Main;
